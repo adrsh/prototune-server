b038000d3dc979ad3794dfdf0662b8c84615979c
Adrian Shosholli <as227cw@student.lnu.se>
2022-04-24 03:57:51 +0200

    Add ability to save instruments and their properties

--- src/server.js
+++ src/server.js
@@ -3,10 +3,11 @@ import WebSocket, { WebSocketServer } from 'ws'
 try {
   const wss = new WebSocketServer({ port: process.env.PORT })
 
+  const instruments = {}
   const storage = {}
 
   wss.on('connection', function connection (ws) {
-    ws.send(Buffer.from(JSON.stringify({ action: 'note-import', notes: storage })))
+    ws.send(Buffer.from(JSON.stringify({ action: 'editor-import', instruments: instruments, rolls: storage })))
     ws.on('message', function message (data) {
       wss.clients.forEach(function each (client) {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -16,20 +17,10 @@ try {
       console.log('received: %s', data)
       const message = JSON.parse(data)
       if (message.action === 'note-create') {
-        if (storage[message.roll]) {
-          storage[message.roll][message.note.uuid] = {
-            x: message.note.x,
-            y: message.note.y,
-            length: message.note.length
-          }
-        } else {
-          storage[message.roll] = {
-            [message.note.uuid]: {
-              x: message.note.x,
-              y: message.note.y,
-              length: message.note.length
-            }
-          }
+        storage[message.roll][message.note.uuid] = {
+          x: message.note.x,
+          y: message.note.y,
+          length: message.note.length
         }
       } else if (message.action === 'note-remove') {
         if (storage[message.roll][message.note.uuid]) {
@@ -41,7 +32,25 @@ try {
             storage[message.roll][message.changes.uuid][key] = value
           }
         }
+      } else if (message.action === 'instrument-create') {
+        storage[message.props.roll] = {}
+        instruments[message.uuid] = {
+          roll: message.props.roll,
+          instrument: message.props.instrument
+        }
+      } else if (message.action === 'instrument-update') {
+        for (const [key, value] of Object.entries(message.props)) {
+          if (key !== 'roll') {
+            instruments[message.uuid][key] = value
+          }
+        }
+      } else if (message.action === 'instrument-remove') {
+        if (instruments[message.uuid]) {
+          delete storage[instruments[message.uuid].roll]
+          delete instruments[message.uuid]
+        }
       }
+      console.log(instruments)
       console.log(storage)
     })
   })
