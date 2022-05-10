f6ec7305dc90341d6ca19de9b5cb4664e34c83ae
Adrian Shosholli <as227cw@student.lnu.se>
2022-05-10 05:38:04 +0200

    Add try-catch for most stuff and add client ping handling

--- src/server.js
+++ src/server.js
@@ -11,50 +11,68 @@ try {
   const sessions = {}
   const clients = {}
 
+  // Handle pings from client.
+  wss.on('connection', ws => {
+    ws.on('message', data => {
+      try {
+        const message = JSON.parse(data)
+        if (message === 'ping' && ws.readyState === WebSocket.OPEN) {
+          ws.send(JSON.stringify('pong'))
+        }
+      } catch (error) {
+        console.error('An error occured')
+      }
+    })
+  })
+
   wss.on('connection', ws => {
     ws.on('message', async data => {
-      const message = JSON.parse(data)
-      if (message.action === 'session-auth') {
-        const session = await Session.findOne({ id: message.id })
-        if (session && session.password === message.password) {
+      try {
+        const message = JSON.parse(data)
+        if (message.action === 'session-auth') {
+          const session = await Session.findOne({ id: message.id })
+          if (session && session.password === message.password) {
           // Add the client to an object keeping check of session clients.
-          if (clients[message.id] && !clients[message.id].has(ws)) {
-            clients[message.id].add(ws)
+            if (clients[message.id] && !clients[message.id].has(ws)) {
+              clients[message.id].add(ws)
+            } else {
+              clients[message.id] = new Set()
+              clients[message.id].add(ws)
+            }
+            // Fetch database session into memory if needed
+            if (!sessions[message.id]) {
+              sessions[session.id] = { password: session.password, instruments: JSON.parse(session.instruments), rolls: JSON.parse(session.rolls) }
+            }
+            // Add session id to Websocket client object
+            ws.id = message.id
+            // Send message to confirm that authentication was successful.
+            ws.send(JSON.stringify({
+              action: 'session-authenticated'
+            }))
           } else {
-            clients[message.id] = new Set()
-            clients[message.id].add(ws)
+            ws.send(JSON.stringify({
+              message: 'authentication-failed'
+            }))
           }
-          // Fetch database session into memory if needed
-          if (!sessions[message.id]) {
-            sessions[session.id] = { password: session.password, instruments: JSON.parse(session.instruments), rolls: JSON.parse(session.rolls) }
-          }
-          // Add session id to Websocket client object
-          ws.id = message.id
-          // Send message to confirm that authentication was successful.
+        } else if (message.action === 'session-create') {
+          const uuid = randomUUID()
+          sessions[uuid] = { password: message.password, instruments: {}, rolls: {} }
+          const session = new Session({
+            id: uuid,
+            password: message.password,
+            instruments: JSON.stringify({}),
+            rolls: JSON.stringify({})
+          })
+          await session.save()
+          clients[uuid] = new Set()
+          clients[uuid].add(ws)
+          ws.id = uuid
           ws.send(JSON.stringify({
-            action: 'session-authenticated'
-          }))
-        } else {
-          ws.send(JSON.stringify({
-            message: 'authentication-failed'
+            'session-id': uuid
           }))
         }
-      } else if (message.action === 'session-create') {
-        const uuid = randomUUID()
-        sessions[uuid] = { password: message.password, instruments: {}, rolls: {} }
-        const session = new Session({
-          id: uuid,
-          password: message.password,
-          instruments: JSON.stringify({}),
-          rolls: JSON.stringify({})
-        })
-        await session.save()
-        clients[uuid] = new Set()
-        clients[uuid].add(ws)
-        ws.id = uuid
-        ws.send(JSON.stringify({
-          'session-id': uuid
-        }))
+      } catch (error) {
+        console.error(error)
       }
     })
   })
@@ -64,7 +82,7 @@ try {
    */
   async function saveSessions () {
     for (const [id, data] of Object.entries(sessions)) {
-      await Session.findOneAndUpdate({ id: id }, {
+      await Session.findOneAndUpdate({ id }, {
         rolls: JSON.stringify(data.rolls),
         instruments: JSON.stringify(data.instruments)
       })
@@ -98,71 +116,75 @@ try {
   /**
    * Save a session with a specific id.
    *
-   * @param {String} id Id of session to save.
+   * @param {string} id Id of session to save.
    */
   async function saveSession (id) {
-    await Session.findOneAndUpdate({ id: id }, {
+    await Session.findOneAndUpdate({ id }, {
       rolls: JSON.stringify(sessions[id].rolls),
       instruments: JSON.stringify(sessions[id].instruments)
     })
   }
 
-  wss.on('connection', function connection (ws) {
-    ws.on('message', function message (data) {
+  wss.on('connection', ws => {
+    ws.on('message', data => {
       console.log('received: %s', data)
       if (ws.id) {
-        const message = JSON.parse(data)
-        for (const client of clients[ws.id].values()) {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify(message))
-          }
-        }
-        if (message.action === 'note-create') {
-          if (sessions[ws.id].rolls[message.roll]) {
-            sessions[ws.id].rolls[message.roll][message.note.uuid] = {
-              x: message.note.x,
-              y: message.note.y,
-              length: message.note.length
+        try {
+          const message = JSON.parse(data)
+          for (const client of clients[ws.id].values()) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify(message))
             }
-          } else {
-            sessions[ws.id].rolls[message.roll] = {
-              [message.note.uuid]: {
+          }
+          if (message.action === 'note-create') {
+            if (sessions[ws.id].rolls[message.roll]) {
+              sessions[ws.id].rolls[message.roll][message.note.uuid] = {
                 x: message.note.x,
                 y: message.note.y,
                 length: message.note.length
               }
+            } else {
+              sessions[ws.id].rolls[message.roll] = {
+                [message.note.uuid]: {
+                  x: message.note.x,
+                  y: message.note.y,
+                  length: message.note.length
+                }
+              }
             }
-          }
-        } else if (message.action === 'note-remove') {
-          if (sessions[ws.id].rolls[message.roll][message.note.uuid]) {
-            delete sessions[ws.id].rolls[message.roll][message.note.uuid]
-          }
-        } else if (message.action === 'note-update') {
-          for (const [key, value] of Object.entries(message.changes)) {
-            if (key !== 'uuid') {
-              sessions[ws.id].rolls[message.roll][message.changes.uuid][key] = value
+          } else if (message.action === 'note-remove') {
+            if (sessions[ws.id].rolls[message.roll][message.note.uuid]) {
+              delete sessions[ws.id].rolls[message.roll][message.note.uuid]
             }
-          }
-        } else if (message.action === 'instrument-create') {
-          sessions[ws.id].rolls[message.props.roll] = {}
-          sessions[ws.id].instruments[message.uuid] = {
-            roll: message.props.roll,
-            instrument: message.props.instrument
-          }
-        } else if (message.action === 'instrument-update') {
-          for (const [key, value] of Object.entries(message.props)) {
-            if (key !== 'roll') {
-              sessions[ws.id].instruments[message.uuid][key] = value
+          } else if (message.action === 'note-update') {
+            for (const [key, value] of Object.entries(message.changes)) {
+              if (key !== 'uuid') {
+                sessions[ws.id].rolls[message.roll][message.changes.uuid][key] = value
+              }
             }
+          } else if (message.action === 'instrument-create') {
+            sessions[ws.id].rolls[message.props.roll] = {}
+            sessions[ws.id].instruments[message.uuid] = {
+              roll: message.props.roll,
+              instrument: message.props.instrument
+            }
+          } else if (message.action === 'instrument-update') {
+            for (const [key, value] of Object.entries(message.props)) {
+              if (key !== 'roll') {
+                sessions[ws.id].instruments[message.uuid][key] = value
+              }
+            }
+          } else if (message.action === 'instrument-remove') {
+            if (sessions[ws.id].instruments[message.uuid]) {
+              const roll = sessions[ws.id].instruments[message.uuid].roll
+              delete sessions[ws.id].rolls[roll]
+              delete sessions[ws.id].instruments[message.uuid]
+            }
+          } else if (message.action === 'session-get') {
+            ws.send(JSON.stringify({ action: 'editor-import', instruments: sessions[ws.id].instruments, rolls: sessions[ws.id].rolls }))
           }
-        } else if (message.action === 'instrument-remove') {
-          if (sessions[ws.id].instruments[message.uuid]) {
-            const roll = sessions[ws.id].instruments[message.uuid].roll
-            delete sessions[ws.id].rolls[roll]
-            delete sessions[ws.id].instruments[message.uuid]
-          }
-        } else if (message.action === 'session-get') {
-          ws.send(JSON.stringify({ action: 'editor-import', instruments: sessions[ws.id].instruments, rolls: sessions[ws.id].rolls }))
+        } catch (error) {
+          console.error('Message contained invalid data: ' + data)
         }
       }
       // console.log(clients)
