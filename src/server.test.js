fe0f2333bdc025f1d2c238c2aadabba4aa57bfea
Adrian Shosholli <as227cw@student.lnu.se>
2022-05-18 02:02:41 +0200

    Add tests for note updating and removal

--- src/server.test.js
+++ src/server.test.js
@@ -73,6 +73,19 @@ test('Note creation with x value less than zero', () => {
   })).toBe(false)
 })
 
+test('Note creation with x value greater than 63', () => {
+  expect(validate({
+    action: 'note-create',
+    note: {
+      x: 64,
+      y: 0,
+      length: 1,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(false)
+})
+
 test('Note creation with y value less than zero', () => {
   expect(validate({
     action: 'note-create',
@@ -197,3 +210,162 @@ test('Note creation with missing roll uuid value', () => {
     }
   })).toBe(false)
 })
+
+test('Note creation with unexpected extra values', () => {
+  expect(validate({
+    action: 'note-create',
+    note: {
+      x: 0,
+      y: 0,
+      length: 1,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7',
+    uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(false)
+})
+
+test('Note creation with unexpected extra values inside note', () => {
+  expect(validate({
+    action: 'note-create',
+    note: {
+      x: 0,
+      y: 0,
+      length: 1,
+      z: 0,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(false)
+})
+
+test('Note removal with valid values', () => {
+  expect(validate({
+    action: 'note-remove',
+    note: {
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(true)
+})
+
+test('Note removal without note uuid', () => {
+  expect(validate({
+    action: 'note-remove',
+    note: {},
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(false)
+})
+
+test('Note removal without roll uuid', () => {
+  expect(validate({
+    action: 'note-remove',
+    note: {
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    }
+  })).toBe(false)
+})
+
+test('Note removal without roll and note', () => {
+  expect(validate({
+    action: 'note-remove'
+  })).toBe(false)
+})
+
+test('Note removal with unexpected note values', () => {
+  expect(validate({
+    action: 'note-remove',
+    note: {
+      x: 0,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(false)
+})
+
+test('Note removal with unexpected values', () => {
+  expect(validate({
+    action: 'note-remove',
+    note: {
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7',
+    uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(false)
+})
+
+test('Note update with valid attributes', () => {
+  expect(validate({
+    action: 'note-update',
+    note: {
+      x: 1,
+      y: 1,
+      length: 2,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(true)
+})
+
+test('Note update with only x value', () => {
+  expect(validate({
+    action: 'note-update',
+    note: {
+      x: 1,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(true)
+})
+
+test('Note update with only y value', () => {
+  expect(validate({
+    action: 'note-update',
+    note: {
+      y: 1,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(true)
+})
+
+test('Note update with only length value', () => {
+  expect(validate({
+    action: 'note-update',
+    note: {
+      length: 2,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(true)
+})
+
+test('Note update without roll uuid', () => {
+  expect(validate({
+    action: 'note-update',
+    note: {
+      length: 2,
+      uuid: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+    }
+  })).toBe(false)
+})
+
+test('Note update with no note attributes', () => {
+  expect(validate({
+    action: 'note-update',
+    note: {},
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(false)
+})
+
+test('Note update without note uuid', () => {
+  expect(validate({
+    action: 'note-update',
+    note: {
+      x: 1
+    },
+    roll: 'efa2765b-dfe0-4476-8220-e70b706421e7'
+  })).toBe(false)
+})
+
+
