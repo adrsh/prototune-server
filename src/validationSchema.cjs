77501b179c10b2b56c5c4024dfba25374946fbf3
Adrian Shosholli <as227cw@student.lnu.se>
2022-05-22 04:28:47 +0200

    Add support for more presets

--- src/validationSchema.cjs
+++ src/validationSchema.cjs
@@ -34,7 +34,7 @@ const schema = {
           properties: {
             instrument: {
               type: 'string',
-              enum: ['piano', 'casio', '808', '909', 'cr78', 'room', 'synth', 'pulse', 'square', 'sine', 'amsynth', 'fmsynth']
+              enum: ['piano', 'casio', '808', '909', 'cr78', 'room', 'synth', 'pulse', 'square', 'sine', 'triangle', 'sawtooth']
             },
             volume: { type: 'number', minimum: -60, maximum: 0 },
             reverb: { type: 'number', minimum: 0, maximum: 1 },
